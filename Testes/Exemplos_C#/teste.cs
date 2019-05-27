f6599f9815f979135b473e425952e82c21615a2d
Nelson Dressler <NelsonDressler@hotmail.com>
2019-05-27 16:36:58 -0300

    Formatação do arquivo teste.cs

--- Testes/Exemplos_C#/teste.cs
+++ Testes/Exemplos_C#/teste.cs
@@ -2,24 +2,30 @@ using System;
 using System.Collections.Generic;
 
 #region HelloWorld
-namespace ConsoleApp {
-    public class Program {
-        public static void Main (string[] args) {
-            System.Console.WriteLine ("Hello World!");
+namespace ConsoleApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
         }
     }
 }
 #endregion
 
-public class Teste {
+public class Teste
+{
     public int Mensagem { get; set; }
 
-    public Teste () {
+    public Teste()
+    {
         Mensagem = "Saudações! Meu nome é Nelson J. Dressler, tenho 28 anos e 11 meses de idade. Sou formado em Ciência da Computação pelo Centro Universitário Senac - São Paulo/SP e em Tecnologia da Informação e Comunicação pelo Instituto Superior de Tecnologia em Ciência da Computação de Petrópolis/RJ.";
     }
 
-    public void Testando () {
-        Console.WriteLine (Mensagem);
+    public void Testando()
+    {
+        Console.WriteLine(Mensagem);
 
         string nome = "Felipe";
         string numero = 123123;
@@ -29,13 +35,14 @@ public class Teste {
             Teste 0oO
         */
 
-        if (nome == numero.ToString ()) {
-            Console.WriteLine ("Teste");
-            Console.WriteLine ("São iguais!");
+        if (nome == numero.ToString())
+        {
+            Console.WriteLine("Teste");
+            Console.WriteLine("São iguais!");
         }
 
-        List<int> codigos = new List<int> ();
-        codigos.Add (4);
-        codigos.Add (5);
+        List<int> codigos = new List<int>();
+        codigos.Add(4);
+        codigos.Add(5);
     }
 }
